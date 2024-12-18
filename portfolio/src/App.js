b86582eaaf1608e7481c0cd479d0eef121966f7c
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-18 15:48:02 +0530

    2

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,9 +1,11 @@
 import Navbar from './Components/NavBar/navbar.js';
+import Intro from './Components/Intro/intro.js'
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro/>
     </div>
   );
 }
--- portfolio/src/Components/NavBar/navbar.js
+++ portfolio/src/Components/NavBar/navbar.js
@@ -1,13 +1,21 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../Assets/Logo1.png'
-import contect_logo from '../../Assets/contect.svg'
+import React from 'react';
+import './navbar.css';
+import logo from '../../Assets/Logo1.png';
+import contect_logo from '../../Assets/contect.svg';
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return (
         <nav className = 'navbar'>
             <img src={logo} alt='logo' className='logo' />
-            <div className='decktopMenu'></div>
+            <div className='decktopMenu'>
+                <Link className='decktopMenuItems'>Home</Link>
+                <Link className='decktopMenuItems'>About</Link>
+                <Link className='decktopMenuItems'>Education</Link>
+                <Link className='decktopMenuItems'>Project</Link>
+                <Link className='decktopMenuItems'>Skills</Link>
+                <Link className='decktopMenuItems'>Positons</Link>
+            </div>
             <button className='contectButton'>
                 <img src={contect_logo} alt='' className='contectIcon' />
                 Contect Me
