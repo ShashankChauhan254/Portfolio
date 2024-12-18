d5ef7a88dfef3029cfb57ec3a47cd72d3bf58247
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-19 01:20:31 +0530

    Push

--- portfolio/src/Components/Intro/intro.js
+++ portfolio/src/Components/Intro/intro.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './intro.css';
-
+import bg from '../../Assets/My.png';
+import ReactTyped from react-typed;
+import { Link } from 'react-scroll';
+import ResumeLogo from '../../Assets/Resume.png'
+import Resume from '../../Assets/Resume.pdf';
 const Intro = () => {
     return (
         <selection id='intro'>
             <div className='IntroContent'>
-
+                <span className='hello'>Hello,</span>
+                <span className='introText'>I'm <span className='introName'>Shashank</span><br /><ReactTyped className='profession' string={["Software Developer", "Game Developer", "ML-DL Engineer", "Web Developer"]} typeSpeed={100} backSpeed={30} loop cursorChar='|' showCoursor={true}/></span>
+                <p className='introPara'></p>
+                <Link><button className='btn'><img src={ResumeLogo} art="Resume" className='ResumeLogo' />Resume</button></Link>
             </div>
             <img src={bg} alt="Portfolio" className='bg' />
         </selection>
