941d7ad9c66ec3282bfbefd0cbbcc24247ff3d3a
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-19 12:42:29 +0530

    CodespaceChange

--- portfolio/src/Components/Intro/intro.js
+++ portfolio/src/Components/Intro/intro.js
@@ -1,18 +1,28 @@
 import React from 'react';
 import './intro.css';
 import bg from '../../Assets/My.png';
-import ReactTyped from 'react-typed';
+import { ReactTyped } from 'react-typed';
 import { Link } from 'react-scroll';
 import ResumeLogo from '../../Assets/Resume.png'
-import Resume from '../../Assets/Resume.pdf';
+
 const Intro = () => {
     return (
         <selection id='intro'>
             <div className='IntroContent'>
-                <span className='hello'>Hello,</span>
-                <span className='introText'>I'm <span className='introName'>Shashank</span><br /><ReactTyped className='profession' string={["Software Developer", "Game Developer", "ML-DL Engineer", "Web Developer"]} typeSpeed={100} backSpeed={30} loop cursorChar='|' showCoursor={true}/></span>
+                <span className='hello'>Hello,<br /></span>
+                <span className='introText'>I'm <span className='introName'>Shashank</span><br />
+                <ReactTyped 
+                  className='profession' 
+                  strings={["Software Developer", "Game Developer", "ML-DL Engineer", "Web Developer"]} 
+                  typeSpeed={100} 
+                  backSpeed={30} 
+                  loop={true} 
+                  cursorChar='|' 
+                  showCursor={true} 
+                />
+                </span>
                 <p className='introPara'></p>
-                <Link><button className='btn'><img src={ResumeLogo} art="Resume" className='ResumeLogo' />Resume</button></Link>
+                <Link><button className='btn'><img src={ResumeLogo} art='' className='ResumeLogo' /><span>Resume</span></button></Link>
             </div>
             <img src={bg} alt="Portfolio" className='bg' />
         </selection>
