461e19b0ffe2101796e603ab738a691bde6a4406
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-26 13:48:22 +0000

    26/12

--- portfolio/src/Components/Intro/intro.js
+++ portfolio/src/Components/Intro/intro.js
@@ -22,7 +22,6 @@ const Intro = () => {
                 />
                 </span>
                 <p className='introPara'></p>
-                <Link><button className='btn'><img src={ResumeLogo} art='' className='ResumeLogo' /><span>Resume</span></button></Link>
                 <Link><button className='btn'><img src={ResumeLogo} art='' className='ResumeLogo' />Resume</button></Link>
             </div>
             <img src={bg} alt="Portfolio" className='bg' />
