ad3c26720a701dd69c82ddf22d64fd881e37491b
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-30 01:55:52 +0530

    30/12

--- src/Components/NavBar/navbar.js
+++ src/Components/NavBar/navbar.js
@@ -1,28 +1,28 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../Assets/Logo1.png'
-import contact_logo from '../../Assets/contact.svg'
-import {Link} from 'react-scroll'
+import React from 'react';
+import './navbar.css';
+import logo from '../../Assets/Logo1.png';
+import contact_logo from '../../Assets/contact.svg';
+import { Link } from 'react-scroll';
 
 const Navbar = () => {
     return (
-        <nav className = 'navbar'>
+        <nav className='navbar'>
             <img src={logo} alt='logo' className='logo' />
             <div className='decktopMenu'>
-                <Link className='decktopMenuItems'>Home</Link>
-                <Link className='decktopMenuItems'>About Me</Link>
-                <Link className='decktopMenuItems'>Education</Link>
-                <Link className='decktopMenuItems'>Skills</Link>
-                <Link className='decktopMenuItems'>Project</Link>
-                <Link className='decktopMenuItems'>Research Paper</Link>
-                <Link className='decktopMenuItems'>Positons</Link>
+                <Link to='intro' smooth={true} duration={500} className='decktopMenuItems'>Home</Link>
+                <Link to='About' smooth={true} duration={500} className='decktopMenuItems'>About Me</Link>
+                <Link to='education' smooth={true} duration={500} className='decktopMenuItems'>Education</Link>
+                <Link to='skills' smooth={true} duration={500} className='decktopMenuItems'>Skills</Link>
+                <Link to='Project' smooth={true} duration={500} className='decktopMenuItems'>Project</Link>
+                <Link to='ResearchPaper' smooth={true} duration={500} className='decktopMenuItems'>Research Paper</Link>
+                <Link to='Positions' smooth={true} duration={500} className='decktopMenuItems'>Positions</Link>
             </div>
-            <button className='contactButton'>
-                <img src={contact_logo} alt='' className='contactIcon' />
+            <Link to='Contact' smooth={true} duration={500} className='contactButton'>
+                <img src={contact_logo} alt='Contact Icon' className='contactIcon' />
                 Contact Me
-            </button>
+            </Link>
         </nav>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
