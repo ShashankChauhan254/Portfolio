c54199e7c804178d9ff8c895002c44724feb9e1c
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2024-12-30 09:58:53 +0530

    Update projects.js
    commented portfolio project

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -13,13 +13,13 @@ const Projects = () => {
                         <a href='https://github.com/ShashankChauhan254/Fruit_Veg_Recognize.git'><h4>GitHub</h4></a>
                     </div>
                 </div>
-                <div className='ProjectBar'>
-                    <div className='ProjectName'>
-                        <h2>Portfolio</h2>
-                        <h4>Dec 2024 - Dec 2024</h4>
-                        <a href='#'><h4>Website</h4></a>
-                    </div>
-                </div>
+                // <div className='ProjectBar'>
+                //     <div className='ProjectName'>
+                //         <h2>Portfolio</h2>
+                //         <h4>Dec 2024 - Dec 2024</h4>
+                //         <a href='#'><h4>Website</h4></a>
+                //     </div>
+                // </div>
                 <div className='ProjectBar'>
                     <div className='ProjectName'>
                         <h2>Shooting Game</h2>
