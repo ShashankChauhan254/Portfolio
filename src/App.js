a84060e6daf23b3aa46d82a6d27d48a69f5844d3
ShashankChauhan254 <132788967+ShashankChauhan254@users.noreply.github.com>
2025-08-03 20:58:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import Navbar from './Components/NavBar/navbar.js';
 import Intro from './Components/Intro/intro.js';
 import About from './Components/About/about.js'
